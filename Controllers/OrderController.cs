f0c82c65445cca04a6f132d67a3117751018c70d
atakandemirrr <atakan1905.demir@gmail.com>
2024-04-26 12:33:06 +0300

    .

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using B2B_Deneme.Models;
+using B2B_Deneme.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace B2B_Deneme.Controllers
+{
+
+    public class OrderController : Controller
+    {
+        public readonly DataContext _context;
+
+        public OrderController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Orders()
+        {
+
+
+            return View();
+        }
+    }
+}
--- wwwroot/js/jsgrid/jsgrid.js
+++ wwwroot/js/jsgrid/jsgrid.js
@@ -1,6 +1,6 @@
 (function($) {
     "use strict";
-    $("#basicScenario").jsGrid({
+    $("#OrdersTable").jsGrid({
         width: "100%",
         filtering: true,
         editing: true,
@@ -10,14 +10,12 @@
         autoload: true,
         pageSize: 15,
         pageButtonCount: 5,
-        deleteConfirm: "Do you really want to delete the client?",
+        deleteConfirm: "Urun Silmek Istediginize Emin Misiniz",
         controller: db,
         fields: [
-        { name: "Name", type: "text", width: 150 },
-        { name: "Age", type: "number", width: 50 },
-        { name: "Address", type: "text", width: 200 },
-        { name: "Country", type: "select", items: db.countries, valueField: "Id", textField: "Name" },
-        { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+        { name: "StokAdi", type: "text", width: 150 },
+        { name: "Adet", type: "number", width: 50 },
+        { name: "Fiyat", type: "text", width: 200 },
         { type: "control" }
         ]
     });
